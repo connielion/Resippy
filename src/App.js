7e522c1322cbbd2a9b6e22f000b7f094db5eaf2f
Connie <clai011@ucr.edu>
2019-06-24 01:35:33 -0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Form from './components/Form'
+import Recipes from './components/Recipes'
 import './App.css';
+require('dotenv').config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '97f1948f1faca2bffb5bd14a0b3d1a74';
+class App extends Component {
+
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const req = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    console.log(process.env);
+    const res = await req.json(); // result
+    this.setState({ recipes: res.recipes });
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h1 className="App-title">Resippy</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
